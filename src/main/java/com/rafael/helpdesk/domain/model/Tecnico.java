efa02341fcc496aa3008d116cc865facfdba40d5
Rafael Cunha <rafaelcn1@hotmail.com>
2023-08-11 11:47:53 -0300

    fix: criado o metodo setPerfis para resolver o JSON estava fornecendo uma lista de objetos Perfil em vez de uma lista de IDs como inteiros.

--- src/main/java/com/rafael/helpdesk/domain/model/Tecnico.java
+++ src/main/java/com/rafael/helpdesk/domain/model/Tecnico.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.rafael.helpdesk.domain.enums.Perfil;
 import com.rafael.helpdesk.dtos.TecnicoDTO;
 
 @Entity
@@ -54,5 +55,10 @@ public class Tecnico extends Pessoa implements Serializable {
 	public void setChamados(List<Chamado> chamados) {
 		this.chamados = chamados;
 	}
+	
+	public void setPerfis(List<Perfil> perfis) {
+	    this.perfis = perfis.stream().map(Perfil::getCodigo).collect(Collectors.toSet());
+	}
+
 
 }
